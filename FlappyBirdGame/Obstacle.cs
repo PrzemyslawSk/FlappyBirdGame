a3e2037cb710668c8c3e307cd0da771715293d11
Przemysław Skoczewski <pskoczew@student.wszib.edu.pl>
2019-05-05 21:57:14 +0200

    git init

---
+++ FlappyBirdGame/Obstacle.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Shapes;
+
+namespace FlappyBirdGame
+{
+    class Obstacle
+    {
+        Rectangle texture;
+        Vector startPosition = new Vector(0, 0);
+        public Obstacle(Rectangle texture, Vector startPosition)
+        {
+            this.startPosition = startPosition;
+            this.texture = texture;
+        }
+
+
+
+    }
+}
---
+++ FlappyBirdGame/Player.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Shapes;
+
+namespace FlappyBirdGame
+{
+    class Player
+    {
+        Rectangle playerImage;
+        public Player(Rectangle playerImage)
+        {
+            this.playerImage = playerImage;
+        }
+
+        void Position()
+        {
+
+        }
+    }
+}
